fb7d08a5f045b4cf1f3d8814b7e3d95545a6f5fc
HarshithaCheekati <168413524+HarshithaCheekati@users.noreply.github.com>
2025-01-06 18:19:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log("start");
+//global scope
+var a=10
+console.log(a);
+
+//local scope
+
+let b =20;
+console.log(b);
+
+let c =30;
+console.log(c);
+console.log("End");
+
+//block scope
+{
+    var m=100;
+console.log(m);
+
+let n =200;
+console.log(n);
+
+let o =300;
+console.log(o);
+
+}
